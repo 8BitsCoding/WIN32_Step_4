653fd1912d4e2ae7a00899ec7d284349e1e02cff
taehyeong77@naver.com <taehyeong77@naver.com>
2019-08-22 01:01:52 +0900

    Commit

---
+++ Project1/Project1.cpp
@@ -0,0 +1,72 @@
+﻿// Project1.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "Project1.h"
+#include "windowsx.h"
+
+int g_is_clicked = 0;
+int g_x = 0, g_y = 0;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_DESTROY) PostQuitMessage(0);
+	else if (uMsg == WM_LBUTTONDOWN) {
+		g_is_clicked = 1;
+		g_x = GET_X_LPARAM(lParam);
+		g_y = GET_Y_LPARAM(lParam);
+
+	}
+	else if (uMsg == WM_LBUTTONUP) {
+		g_is_clicked = 0;
+	}
+	else if (uMsg == WM_MOUSEMOVE) {
+		if (g_is_clicked == 1) {
+			HDC h_dc = GetDC(hWnd);
+
+			MoveToEx(h_dc, g_x, g_y, NULL);
+			int x = GET_X_LPARAM(lParam);
+			int y = GET_Y_LPARAM(lParam);
+			LineTo(h_dc, x, y);
+
+			g_x = x;
+			g_y = y;
+
+			ReleaseDC(hWnd, h_dc);
+		}
+	}
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASS wc;
+
+	wchar_t my_class_name[] = L"tipssoft";
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = my_class_name;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(&wc);
+
+	HWND hWnd = CreateWindow(my_class_name, L"www.tipssoft.com",
+		WS_OVERLAPPEDWINDOW, 100, 90, 400, 350, NULL, NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
